bbf26a448dae27cecfd249dfe8d815902668d392
서윤오 <seo0jjjjj@gmail.com>
2024-04-17 09:01:17 +0900

    feat: 디렉토리에 속한 파일 검색하는 함수추가

--- src/main/java/kr/moble/java/mobleplayer/MoblePlayerApplication.java
+++ src/main/java/kr/moble/java/mobleplayer/MoblePlayerApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import kr.moble.java.mobleplayer.service.FileService;
 
 import java.io.IOException;
 
--- src/main/java/kr/moble/java/mobleplayer/service/FileService.java
+++ src/main/java/kr/moble/java/mobleplayer/service/FileService.java
@@ -5,6 +5,13 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 파일 또는 폴더를 열어 경로를 반환하는 클래스
@@ -41,9 +48,31 @@ public class FileService {
     }
 
 
+    /**
+     * FileChooser 을 생성할 때, 옵션을 줄 수 있는 ExtensionFilter를 초기화하고, FilerChooser 객체를 반환하는 함수
+     * @param filters 파일 옵션에 대해 지정함
+     * @return FileChooser
+     */
     private FileChooser setupFileChooser(FileChooser.ExtensionFilter... filters) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(filters);
         return fileChooser;
+    }
+
+    /**
+     * 해당 폴더 경로에 해당하는 MP3 파일들을 List로 반환
+     * @param directoryPath 탐색할 폴더 경로
+     * @return List<String> MP3파일 리스트
+     */
+    public List<String> searchMp3FilesInDirectory(String directoryPath) {
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".mp3")) // MP3 파일인지
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("MediaPlayerService(70) 디렉토리 파일 읽기 불가");
+        }
     }
 }
